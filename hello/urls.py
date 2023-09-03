6a3112557482d4262ed9e0491513227c9bd15bc1
Göksu ULUDAĞ <goksu.uludag@ekinokssoftware.com>
2023-09-03 18:05:11 +0300

    first commit

---
+++ hello/urls.py
@@ -0,0 +1,35 @@
+from django.urls import include, path
+from hello import views
+from hello.models import LogMessage
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+home_list_view = views.HomeListView.as_view(
+    queryset=LogMessage.objects.order_by("-log_date")[:5],  # :5 limits the results to the five most recent
+    context_object_name="message_list",
+    template_name="hello/home.html",
+)
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
+    #path("", home_list_view, name="home"),
+    path("hello/<name>", views.hello_there, name="hello_there"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("log/", views.log_message, name="log"),
+]
